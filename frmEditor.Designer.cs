2ce696febd1f1bce28d2a623f2016e849d988f12
dibon.francesco03@gmail.com <dibon.francesco03@gmail.com>
2020-11-24 17:10:49 +0100

    editor v1

---
+++ frmEditor.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace EditorSnake
+{
+    partial class frmEditor
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTrasferello = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pnlGestioneBottoni = new System.Windows.Forms.Panel();
+            this.pnlGestioneBottoni.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTrasferello
+            // 
+            this.btnTrasferello.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnTrasferello.Location = new System.Drawing.Point(0, 0);
+            this.btnTrasferello.Name = "btnTrasferello";
+            this.btnTrasferello.Size = new System.Drawing.Size(428, 169);
+            this.btnTrasferello.TabIndex = 0;
+            this.btnTrasferello.Text = "Trasferello";
+            this.btnTrasferello.UseVisualStyleBackColor = true;
+            this.btnTrasferello.Click += new System.EventHandler(this.btnTrasferello_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnReset.Location = new System.Drawing.Point(914, 0);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(491, 169);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // pnlGestioneBottoni
+            // 
+            this.pnlGestioneBottoni.Controls.Add(this.btnReset);
+            this.pnlGestioneBottoni.Controls.Add(this.btnTrasferello);
+            this.pnlGestioneBottoni.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlGestioneBottoni.Location = new System.Drawing.Point(0, 660);
+            this.pnlGestioneBottoni.Name = "pnlGestioneBottoni";
+            this.pnlGestioneBottoni.Size = new System.Drawing.Size(1405, 169);
+            this.pnlGestioneBottoni.TabIndex = 2;
+            // 
+            // frmEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1405, 829);
+            this.Controls.Add(this.pnlGestioneBottoni);
+            this.Name = "frmEditor";
+            this.Text = "EditorSnake";
+            this.Load += new System.EventHandler(this.frmEditor_Load);
+            this.SizeChanged += new System.EventHandler(this.frmEditor_SizeChanged);
+            this.pnlGestioneBottoni.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTrasferello;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Panel pnlGestioneBottoni;
+    }
+}
+
---
+++ frmEditor.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditorSnake
+{
+    public partial class frmEditor : Form
+    {
+        //25, 17 è la dimensione del campo medio
+        private int[,] mat = new int[25, 17];
+        private int sizeButton = 64;
+        public frmEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEditor_Load(object sender, EventArgs e)
+        {
+            InizializzaMatriceCampo(GetMatWidth(), GetMatHeight());
+            GeneraCampo(GetMatWidth(), GetMatHeight());
+            ResizeButtons();
+        }
+
+        private void InizializzaMatriceCampo(int width, int height)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    mat[i, j] = 0;
+                }
+            }
+        }
+
+        private void GeneraCampo(int width, int height)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    int x = i;
+                    int y = j;
+                    Button b = new Button();
+                    b.Enabled = true;
+                    b.Visible = true;
+                    b.Location = new Point(i * sizeButton, j * sizeButton);
+                    b.Size = new Size(sizeButton, sizeButton);
+                    b.Text = i.ToString() + " " + j.ToString();
+                    b.BackColor = Color.Black;
+                    b.Click += delegate {
+                        if (mat[x, y] == 0)
+                        {
+                            mat[x, y] = 1;
+                            b.BackColor = Color.White;
+                        }
+                        else
+                        {
+                            mat[x, y] = 0;
+                            b.BackColor = Color.Black;
+                        }
+                    };
+                    this.Controls.Add(b);
+                }
+            }
+        }
+
+        private int GetMatWidth()
+        {
+            return mat.GetLength(0);
+        }
+
+        private int GetMatHeight()
+        {
+            return mat.GetLength(1);
+        }
+
+        private void btnTrasferello_Click(object sender, EventArgs e)
+        {
+            Console.Write("\n");
+            for (int i = 0; i < GetMatWidth(); i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("\n");
+
+            for (int j = 0; j < GetMatHeight(); j++)
+            {
+                for (int i = 0; i < GetMatWidth(); i++)
+                {
+                    Console.Write(mat[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        private void ResizeButtons()
+        {
+            btnTrasferello.Size = new Size(pnlGestioneBottoni.Size.Width / 2, pnlGestioneBottoni.Size.Height);
+            btnReset.Size = new Size(pnlGestioneBottoni.Size.Width / 2, pnlGestioneBottoni.Size.Height);
+        }
+
+        private void RipristinaCampo()
+        {
+            for (int i = 0; i < GetMatWidth(); i++)
+            {
+                for (int j = 0; j < GetMatHeight(); j++)
+                {
+                    if (mat[i, j] != 0)
+                    {
+                        
+                    }
+                }
+            }
+        }
+
+        private void frmEditor_SizeChanged(object sender, EventArgs e)
+        {
+            ResizeButtons();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Vuoi davvero ripristinare tutto il campo gioco? Le modifiche non salvate andranno perdute",
+                "Conferma",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                /*
+                InizializzaMatriceCampo(GetMatWidth(), GetMatHeight());
+                GeneraCampo(GetMatWidth(), GetMatHeight());
+                */
+
+                RipristinaCampo();
+            }
+        }
+    }
+}
